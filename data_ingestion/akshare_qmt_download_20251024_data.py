4ba45f60904bd1b0b6d18bd6a68e8150b633b3fb
hongjay <hongjie.zhou@melot.cn>
2026-02-08 22:18:52 +0800

    数据依赖移动位置

--- data_ingestion/akshare_qmt_download_20251024_data.py
+++ data_ingestion/akshare_qmt_download_20251024_data.py
@@ -68,7 +68,7 @@ def get_all_stock_codes():
         return []
 
 
-def download_stock_data_for_date(stock_codes, target_date="20251030"):
+def download_stock_data_for_date(stock_codes, target_date="20260203"):
     """
     使用miniqmt下载指定日期的股票数据
     """
@@ -79,7 +79,7 @@ def download_stock_data_for_date(stock_codes, target_date="20251030"):
     failed_stocks = []
 
     # 假设你想从第 2230 个股票开始下载
-    start_item_number = 2500
+    start_item_number = 2750
 
     # Python 索引从 0 开始，所以第 2230 个元素的索引是 2229
     start_index = start_item_number - 1
@@ -101,8 +101,8 @@ def download_stock_data_for_date(stock_codes, target_date="20251030"):
             data = xtdata.download_history_data2(
                 stock_list=[miniqmt_code],
                 period='1d',
-                start_time="20251231",  # 目标日期的前一天
-                end_time="20260105",  # 目标日期的后一天
+                start_time="20260202",  # 目标日期的前一天
+                end_time="20260206",  # 目标日期的后一天
                 incrementally=True,
                 callback=lambda data: print(f"  -> 进度: {data['finished']}/{data['total']} {data['stockcode']}")
             )
@@ -264,17 +264,17 @@ def main():
 
     # 步骤2: 下载数据
     # 【已修改】日期更新为 30 号
-    print(f"\n步骤2: 下载2025年10月30日数据...")
-    success_count, failed_stocks = download_stock_data_for_date(stock_codes, "20260105")
+    # print(f"\n步骤2: 下载2025年10月30日数据...")
+    # success_count, failed_stocks = download_stock_data_for_date(stock_codes, "20260105")
 
     # 步骤3: 更新parquet文件
     # 【已修改】日期更新为 30 号
     print(f"\n步骤3: 更新parquet缓存文件...")
-    update_parquet_files_with_miniqmt_data("20260105")
-
-    print("\n" + "=" * 50)
-    print("数据下载和更新任务完成！")
-    print("=" * 50)
+    update_parquet_files_with_miniqmt_data("20260205")
+    #
+    # print("\n" + "=" * 50)
+    # print("数据下载和更新任务完成！")
+    # print("=" * 50)
 
 
 if __name__ == "__main__":
